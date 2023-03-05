43110046268f15d592597e0c9f5193d5b77eb595
romann-broque <kinoide56@gmail.com>
2023-03-05 23:42:09 +0100

    kill signal

--- srcs/client.c
+++ srcs/client.c
@@ -6,13 +6,18 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 23:17:44 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/05 23:18:07 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/05 23:26:33 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	ft_printf("This is client !\n");
+	if (ac > 1)
+	{
+		kill(ft_atoi(av[1]), SIGUSR1);
+	}
+	return (EXIT_SUCCESS);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 21:39:01 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/05 23:12:00 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/05 23:39:29 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 void	sig_handler(int signum)
 {
-	ft_printf("Signal : %d\n", signum);
+	ft_printf("Signal incoming: %d\n", signum);
 }
 
 int	main(void)
 {
+	ft_printf("This is server !\n");
 	ft_printf("pid -> %d\n", getpid());
-	signal(SIGUSR1, sig_handler);
-	ft_printf("hello !");
+	while (true)
+	{
+		signal(SIGUSR1, sig_handler);
+	}
 	return (EXIT_SUCCESS);
 }
