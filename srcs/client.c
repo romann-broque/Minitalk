c77f81549837f449c57d50ddab630ffddda06633
romann-broque <kinoide56@gmail.com>
2023-03-06 21:26:57 +0100

    in progress

--- srcs/client.c
+++ srcs/client.c
@@ -6,18 +6,56 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 23:17:44 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/05 23:26:33 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/06 21:08:55 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_signal(const int pid, const int sig, const char *sig_name)
+{
+	ft_printf("Send %s\n", sig_name);
+	kill(pid, sig);
+}
+
+void	send_char(const int pid, const char c)
+{
+	int	sig;
+
+	sig = c;
+	// printf("sig -> %b\n", sig);
+	while (sig != 0)
+	{
+		if (sig & 1)
+			send_signal(pid, SIGUSR1, "SIGUSR1");
+		else
+			send_signal(pid, SIGUSR2, "SIGUSR2");
+		pause();
+		sig = (sig >> 1);
+		//printf("sig -> %b\n", sig);
+	}
+	ft_printf("char -> %c = %d\n", c, c);
+}
+
+void	send_str(const int pid, const char *str)
+{
+	while (*str != '\0')
+	{
+		send_char(pid, *str);
+		++str;
+	}
+}
+
 int	main(int ac, char **av)
 {
+	int	pid;
+
 	ft_printf("This is client !\n");
-	if (ac > 1)
+	ft_printf("pid -> %d\n", getpid());
+	if (ac > 2)
 	{
-		kill(ft_atoi(av[1]), SIGUSR1);
+		pid = ft_atoi(av[1]);
+		send_str(pid, av[2]);
 	}
 	return (EXIT_SUCCESS);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,24 +6,60 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 21:39:01 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/05 23:39:29 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/06 21:25:11 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	sig_handler(int signum)
+// void	sig_handler(int signum)
+// {
+// 	ft_printf("Signal incoming: %d\n", signum);
+// 	kill(pid, SIGUSR1);
+// }
+
+// Signal handler function
+void	signal_handler(int signal_number, siginfo_t *siginfo, void *context)
 {
-	ft_printf("Signal incoming: %d\n", signum);
+	const pid_t	client_pid = siginfo->si_pid;
+	const int	client_data = siginfo->si_value.sival_int;
+	printf("Received signal %d from client %d with data %d\n", signal_number, client_pid, client_data);
+	kill(client_pid, SIGUSR1);
+	(void)context;
 }
 
 int	main(void)
 {
+	struct sigaction	sigact;
+
 	ft_printf("This is server !\n");
 	ft_printf("pid -> %d\n", getpid());
+
+	sigemptyset(&sigact.sa_mask);
+	sigaddset(&sigact.sa_mask, SIGUSR1);
+	sigaddset(&sigact.sa_mask, SIGUSR2);
+	sigact.sa_sigaction = signal_handler;
+	sigact.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sigact, NULL);
+	sigaction(SIGUSR2, &sigact, NULL);
 	while (true)
-	{
-		signal(SIGUSR1, sig_handler);
-	}
+		continue;
 	return (EXIT_SUCCESS);
 }
+
+// int	main(void)
+// {
+// 	struct sigaction	sigact;
+
+// 	sigemptyset(&sigact.sa_mask);
+// 	sigaddset(&sigact.sa_mask, SIGUSR1);
+// 	sigaddset(&sigact.sa_mask, SIGUSR2);
+// 	sigact.sa_handler = sig_handler;
+// 	sigaction(SIGUSR1, &sigact, NULL);
+// 	sigaction(SIGUSR2, &sigact, NULL);
+// 	ft_printf("This is server !\n");
+// 	ft_printf("pid -> %d\n", getpid());
+// 	while (true)
+// 		continue ;
+// 	return (EXIT_SUCCESS);
+// }
