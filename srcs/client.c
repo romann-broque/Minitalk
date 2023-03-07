a964fa4741c3191dfcd9fc1d2343f74bea48b484
romann-broque <kinoide56@gmail.com>
2023-03-07 14:20:21 +0100

    signal answer

--- srcs/client.c
+++ srcs/client.c
@@ -6,31 +6,11 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 23:17:44 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/07 13:33:42 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/07 14:17:05 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-void	waiting_for_signal(void)
-{
-	pause();
-	// struct sigaction	sigact;
-
-	// sigemptyset(&sigact.sa_mask);
-	// sigaddset(&sigact.sa_mask, SIGUSR1);
-	// sigact.sa_sigaction = signal_received;
-	// sigact.sa_flags = SA_SIGINFO;
-	// sigaction(SIGUSR1, &sigact, NULL);
-	// sigaction(SIGUSR2, &sigact, NULL);
-}
-
-void	send_signal(const int pid, const int sig, const char *sig_name)
-{
-	ft_printf("Send %s\n", sig_name);
-	kill(pid, sig);
-	waiting_for_signal();
-}
 
 void	signal_received(int signal_number, siginfo_t *siginfo, void *context)
 {
@@ -38,8 +18,27 @@ void	signal_received(int signal_number, siginfo_t *siginfo, void *context)
 	const int	server_data = siginfo->si_value.sival_int;
 
 	// check if the incoming sig is from server
-	printf("Received signal %d from server %d with data %d\n", signal_number, server_pid, server_data);
+	ft_printf("Received signal %d from server %d with data %d\n", signal_number, server_pid, server_data);
 	(void)context;
+}
+
+void	waiting_for_signal(const int signum)
+{
+	struct sigaction	sigact;
+
+	sigemptyset(&sigact.sa_mask);
+	sigaddset(&sigact.sa_mask, signum);
+	sigact.sa_sigaction = signal_received;
+	sigact.sa_flags = SA_SIGINFO;
+	sigaction(signum, &sigact, NULL);
+	pause();
+}
+
+void	send_signal(const int pid, const int sig, const char *sig_name)
+{
+	ft_printf("Send %s\n", sig_name);
+	kill(pid, sig);
+	waiting_for_signal(sig);
 }
 
 void	send_char(const int pid, const char c)
@@ -47,7 +46,6 @@ void	send_char(const int pid, const char c)
 	int	sig;
 
 	sig = c;
-	// printf("sig -> %b\n", sig);
 	while (sig != 0)
 	{
 		if (sig & 1)
@@ -55,9 +53,7 @@ void	send_char(const int pid, const char c)
 		else
 			send_signal(pid, SIGUSR2, "SIGUSR2");
 		sig = (sig >> 1);
-		//printf("sig -> %b\n", sig);
 	}
-	ft_printf("char -> %c = %d\n", c, c);
 }
 
 void	send_str(const int pid, const char *str)
--- srcs/server.c
+++ srcs/server.c
@@ -6,25 +6,18 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 21:39:01 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/07 13:37:53 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/07 14:18:08 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// void	sig_handler(int signum)
-// {
-// 	ft_printf("Signal incoming: %d\n", signum);
-// 	kill(pid, SIGUSR1);
-// }
-
-// Signal handler function
 void	signal_answer(int signal_number, siginfo_t *siginfo, void *context)
 {
 	const pid_t	client_pid = siginfo->si_pid;
 	const int	client_data = siginfo->si_value.sival_int;
 
-	printf("Received signal %d from client %d with data %d\n", signal_number, client_pid, client_data);
+	ft_printf("Received signal %d from client %d with data %d\n", signal_number, client_pid, client_data);
 	kill(client_pid, signal_number);
 	(void)context;
 }
@@ -36,11 +29,11 @@ int	main(void)
 	ft_printf("This is server !\n");
 	ft_printf("pid -> %d\n", getpid());
 
+	sigact.sa_sigaction = signal_answer;
+	sigact.sa_flags = SA_SIGINFO;
 	sigemptyset(&sigact.sa_mask);
 	sigaddset(&sigact.sa_mask, SIGUSR1);
 	sigaddset(&sigact.sa_mask, SIGUSR2);
-	sigact.sa_sigaction = signal_answer;
-	sigact.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sigact, NULL);
 	sigaction(SIGUSR2, &sigact, NULL);
 	while (true)
