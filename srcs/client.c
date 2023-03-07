256095c804a4966374f875832d285f0570c8f03f
romann-broque <kinoide56@gmail.com>
2023-03-07 13:40:27 +0100

    in progress

--- srcs/client.c
+++ srcs/client.c
@@ -6,16 +6,40 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 23:17:44 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/06 21:08:55 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/07 13:33:42 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	waiting_for_signal(void)
+{
+	pause();
+	// struct sigaction	sigact;
+
+	// sigemptyset(&sigact.sa_mask);
+	// sigaddset(&sigact.sa_mask, SIGUSR1);
+	// sigact.sa_sigaction = signal_received;
+	// sigact.sa_flags = SA_SIGINFO;
+	// sigaction(SIGUSR1, &sigact, NULL);
+	// sigaction(SIGUSR2, &sigact, NULL);
+}
+
 void	send_signal(const int pid, const int sig, const char *sig_name)
 {
 	ft_printf("Send %s\n", sig_name);
 	kill(pid, sig);
+	waiting_for_signal();
+}
+
+void	signal_received(int signal_number, siginfo_t *siginfo, void *context)
+{
+	const pid_t	server_pid = siginfo->si_pid;
+	const int	server_data = siginfo->si_value.sival_int;
+
+	// check if the incoming sig is from server
+	printf("Received signal %d from server %d with data %d\n", signal_number, server_pid, server_data);
+	(void)context;
 }
 
 void	send_char(const int pid, const char c)
@@ -30,7 +54,6 @@ void	send_char(const int pid, const char c)
 			send_signal(pid, SIGUSR1, "SIGUSR1");
 		else
 			send_signal(pid, SIGUSR2, "SIGUSR2");
-		pause();
 		sig = (sig >> 1);
 		//printf("sig -> %b\n", sig);
 	}
--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: rbroque <rbroque@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 21:39:01 by rbroque           #+#    #+#             */
-/*   Updated: 2023/03/06 21:25:11 by rbroque          ###   ########.fr       */
+/*   Updated: 2023/03/07 13:37:53 by rbroque          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,13 @@
 // }
 
 // Signal handler function
-void	signal_handler(int signal_number, siginfo_t *siginfo, void *context)
+void	signal_answer(int signal_number, siginfo_t *siginfo, void *context)
 {
 	const pid_t	client_pid = siginfo->si_pid;
 	const int	client_data = siginfo->si_value.sival_int;
+
 	printf("Received signal %d from client %d with data %d\n", signal_number, client_pid, client_data);
-	kill(client_pid, SIGUSR1);
+	kill(client_pid, signal_number);
 	(void)context;
 }
 
@@ -38,12 +39,12 @@ int	main(void)
 	sigemptyset(&sigact.sa_mask);
 	sigaddset(&sigact.sa_mask, SIGUSR1);
 	sigaddset(&sigact.sa_mask, SIGUSR2);
-	sigact.sa_sigaction = signal_handler;
+	sigact.sa_sigaction = signal_answer;
 	sigact.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sigact, NULL);
 	sigaction(SIGUSR2, &sigact, NULL);
 	while (true)
-		continue;
+		continue ;
 	return (EXIT_SUCCESS);
 }
 
